2af3c60caaf7650e9d6c9dff46601d8c23d05914
Pedro Howat <pedro.howat@gmail.com>
2014-05-06 11:53:09 -0300

    fixing our resolv.conf when using both work internet connections

--- scripts/sudo-startup-scripts.sh
+++ scripts/sudo-startup-scripts.sh
@@ -3,6 +3,6 @@
 route add default gw 192.168.25.1
 route del default gw 192.168.0.202
 
-# EDIT resolv.conf
+echo "nameserver 192.168.0.202" | cat - /etc/resolv.conf > /tmp/out && mv /tmp/out /etc/resolv.conf
 
 service openvpn restart
