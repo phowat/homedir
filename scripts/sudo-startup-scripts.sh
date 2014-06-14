4b6fad44b667dc422f653dbe7dfe0b79dd0bf61a
Pedro Howat <pedro.howat@gmail.com>
2014-06-13 23:22:33 -0300

    only update resol.conf if we actually need to

--- scripts/sudo-startup-scripts.sh
+++ scripts/sudo-startup-scripts.sh
@@ -3,6 +3,10 @@
 route add default gw 192.168.25.1
 route del default gw 192.168.0.202
 
-echo "nameserver 192.168.0.202" | cat - /etc/resolv.conf > /tmp/out && mv /tmp/out /etc/resolv.conf
+x=`grep "192.168.0.202" /etc/resolv.conf | wc -l`
+if [ "$x" -eq "0" ]
+then
+	echo "nameserver 192.168.0.202" | cat - /etc/resolv.conf > /tmp/out && mv /tmp/out /etc/resolv.conf
+fi
 
 service openvpn restart
