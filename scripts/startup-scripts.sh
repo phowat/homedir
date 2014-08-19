364a4d23935d600a1d3a1929debf8dd08cb8031a
Pedro Howat <pedro.howat@gmail.com>
2014-08-19 20:36:19 -0300

    dont activate uowm by default

--- scripts/startup-scripts.sh
+++ scripts/startup-scripts.sh
@@ -5,7 +5,7 @@
 
 #xrandr --output HDMI1 --left-of VGA1 --mode 1680x1050
 #xrandr --output VGA1 --right-of HDMI1 --mode 1600x900
-#setxkbmap us -variant intl
+setxkbmap us -variant intl -option nodeadkeys
 xmodmap -e 'keycode 66 = Control_L'
 xmodmap -e 'clear Lock'
 xmodmap -e 'add Control = Control_L'
@@ -15,4 +15,5 @@ xmodmap -e 'add Control = Control_L'
 #DISPLAY=:0 /home/pedro/scripts/background.pl
 #/home/pedro/scripts/set-crontab.py
 
-. ~/venv/bin/activate ; /home/pedro/Code/uowm/uowm.py --cli
+#. ~/venv/bin/activate ; /home/pedro/Code/uowm/uowm.py /home/pedro/wallpapers/sfw
+#. ~/venv/bin/activate ; /home/pedro/Code/uowm/uowm.py --cli
