50fb7aab2f62d3b4a70684745d086046bc42c395
Pedro Howat <pedro.howat@gmail.com>
2014-09-24 18:11:04 -0300

    toggle mute bash script

--- scripts/toggle-mute.sh
+++ scripts/toggle-mute.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
-amixer get Master | grep -o '\[o.*\]'
+sound=`amixer get Master | grep -o '\[o.*\]'`
+
+amixer set Master toggle  > /dev/null
+if [ $sound = "[on]" ];
+then 
+	pactl set-sink-mute 0 1
+else
+	pactl set-sink-mute 0 0
+fi
